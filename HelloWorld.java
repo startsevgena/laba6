6fe5a66031bfe947025b15df497089c5e7f62308
Startsev Gennadiy <startsev@gmail.com>
2013-12-11 19:16:28 +0600

    Файл
    Файл из книги по Java.

---
+++ HelloWorld.java
@@ -0,0 +1,26 @@
+/*
+* <applet code="HelloWorld" width=200 height=100>
+* </applet>
+*
+*/
+import java.applet.*;
+import java.awt.*;
+public class HelloWorld extends Applet {
+final Font f = new Font("Helvetica", Font.BOLD, 18);
+
+public void paint(Graphics g) { 
+Dimension d = this.size();
+g.setColor(Color.white);
+g.fillRect(0,0,d.width,d.height);
+g.setColor(Color.black);
+g.setFont(f);
+drawCenteredString("Hello World!", d.width, d.height, g);
+g.drawRect(0,0,d.width-1,d.height-1);
+}
+
+public void drawCenteredString(String s, int w, int h, Graphics g) { 
+FontMetrics fm = g.getFontMetrics();
+int x = (w - fm.stringWidth(s)) / 2;
+int y = (fm.getAscent() + (h - (fm.getAscent() + fm.getDescent()))/2);
+g.drawString(s, x, y);
+} }
