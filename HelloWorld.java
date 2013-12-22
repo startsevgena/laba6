7804a6f3416bc070a71b81ed39bb023897e0ff21
125887 <125887@192.168.1.135>
2013-12-22 14:48:11 +0400

    125887

--- HelloWorld.java
+++ HelloWorld.java
@@ -14,7 +14,7 @@ g.setColor(Color.white);
 g.fillRect(0,0,d.width,d.height);
 g.setColor(Color.black);
 g.setFont(f);
-drawCenteredString("123456", d.width, d.height, g);
+drawCenteredString("125887", d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
 }
 
