1f2f3c4699794cac1995e4c36919660272c6a44a
student <student@GAMMA1.ASU.local>
2013-12-26 16:29:43 +0600

    ввы

--- HelloWorld.java
+++ HelloWorld.java
@@ -14,7 +14,7 @@ g.setColor(Color.white);
 g.fillRect(0,0,d.width,d.height);
 g.setColor(Color.black);
 g.setFont(f);
-drawCenteredString("Исламгареев", d.width, d.height, g);
+drawCenteredString("РСЃР»Р°РјРіР°СЂРµРµРІ", d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
 }
 
