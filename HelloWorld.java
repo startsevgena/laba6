7714c864d2ae6362da47cc8450854b023aca186c
125401 <albina_akhtyamovaa@mail.ru>
2014-01-14 19:53:32 +0300

    Изменение номера зачетки на 125401

--- HelloWorld.java
+++ HelloWorld.java
@@ -14,7 +14,7 @@ g.setColor(Color.white);
 g.fillRect(0,0,d.width,d.height);
 g.setColor(Color.black);
 g.setFont(f);
-drawCenteredString("125897", d.width, d.height, g);
+drawCenteredString("125401", d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
 }
 
