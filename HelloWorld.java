5ab9b95128248b9fd01b19a0b8a9e723b861f288
AlenaGuseva <alyona.guseva1995@yandex.ru>
2014-01-14 09:20:15 -0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -15,7 +15,7 @@ g.fillRect(0,0,d.width,d.height);
 g.setColor(Color.black);
 g.setFont(f);
 drawCenteredString("125401"
-	  	 + "125408", d.width, d.height, g);
+	    + "125408", d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
 }
 
