0c299f30802dce4846cbe25d2fd2346b85069298
ilnar70 <islamgareev_ilna@mail.ru>
2014-01-14 20:29:26 +0300

    Добавление номера зачетки 125413

--- HelloWorld.java
+++ HelloWorld.java
@@ -15,7 +15,8 @@ g.fillRect(0,0,d.width,d.height);
 g.setColor(Color.black);
 g.setFont(f);
 drawCenteredString("125401"
-		 + "125408", d.width, d.height, g);
+		 + "125408"
+		 + "125413", d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
 }
 
