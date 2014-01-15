1699ebc81cbe412f91198ca35e380e478d4d1cce
123079 <CaK@SuperBob>
2014-01-15 18:31:05 +0600

    +123079

--- HelloWorld.java
+++ HelloWorld.java
@@ -15,7 +15,7 @@ g.fillRect(0,0,d.width,d.height);
 g.setColor(Color.black);
 g.setFont(f);
 drawCenteredString("123030"
-		+"123079", d.width, d.height, g);
+		  +"123079", d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
 }
 
