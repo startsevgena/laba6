a63145d4d449a2b2cf24c466276a804735268b0f
Loa <Loa@ANZU>
2014-01-18 17:45:40 +0600

    1

--- HelloWorld.java
+++ HelloWorld.java
@@ -14,7 +14,7 @@ g.setColor(Color.white);
 g.fillRect(0,0,d.width,d.height);
 g.setColor(Color.black);
 g.setFont(f);
-drawCenteredString("125415"
+drawCenteredString("Капп"
 		+ "125855"
 		+ "125423"
 		+ "125428", d.width, d.height, g);
