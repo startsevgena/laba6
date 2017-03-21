136e80746d800c72c7837d6a7ef3ad7c5546c9d1
DamirGilmanov <damir_str_95@mail.ru>
2017-03-21 21:49:14 +0500

    Второй участник внес изменения (135541).

--- HelloWorld.java
+++ HelloWorld.java
@@ -15,7 +15,7 @@ g.fillRect(0,0,d.width,d.height);
 g.setColor(Color.black);
 g.setFont(f);
 drawCenteredString("145805"
-		+ "133007"
+		+ "135541"
 		+ "133008"
 		+ "133009", d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
