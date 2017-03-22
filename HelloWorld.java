241652f53b345e50e93a962f690930d4e172514a
blekdeat <blekdeat@mail.ru>
2017-03-22 15:24:01 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -17,7 +17,7 @@ g.setFont(f);
 drawCenteredString("15130762"
 		+ "133090"
 		+ "146091"
-		+ "133009", d.width, d.height, g);
+		+ "15130883", d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
 }
 
