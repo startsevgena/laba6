2a765f3761965e775cbbae58c076ba041c9a872d
yukidji <yukbaeva@gmail.com>
2017-03-28 18:49:06 +0500

    добавление номера зачетки

--- HelloWorld.java
+++ HelloWorld.java
@@ -16,7 +16,7 @@ g.setColor(Color.black);
 g.setFont(f);
 drawCenteredString("05102016"
 		+ "15130758"
-		+ "133008"
+		+ "15130565"
 		+ "133009", d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
 }
