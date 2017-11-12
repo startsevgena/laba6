573c153ff04c813ff21abd22efafaaa43d61c24f
carolineadderiy <carolineadderiy@gmail.com>
2017-11-12 19:22:18 +0500

    Номер зач. книжки добавлен третьим студентом

--- HelloWorld.java
+++ HelloWorld.java
@@ -16,7 +16,7 @@ g.setColor(Color.black);
 g.setFont(f);
 drawCenteredString("16130031"
 		+ "16130172"
-		+ "133008"
+		+ "16130702"
 		+ "133009", d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
 }
