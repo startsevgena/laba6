6ab6dd43fdeffa51cdbfd581a6a11792689148c9
unknown <yo.98@mail.ru>
2017-11-21 23:28:29 +0500

    Добавлен номер зачётной книжки четвёртого студента

--- HelloWorld.java
+++ HelloWorld.java
@@ -17,7 +17,7 @@ g.setFont(f);
 drawCenteredString("16130567"
 		+ "16130569"
 		+ "16130163"
-		+ "133009", d.width, d.height, g);
+		+ "16130029", d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
 }
 
