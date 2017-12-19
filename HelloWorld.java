bc08fae3d6462e269cd3aba69e114b090608e024
ShirgalinRenat <shirgali.renat@mail.ru>
2017-12-19 23:56:42 +0500

    ПЕРВЫЙ студент добавил номер ЗК

--- HelloWorld.java
+++ HelloWorld.java
@@ -14,10 +14,9 @@ g.setColor(Color.white);
 g.fillRect(0,0,d.width,d.height);
 g.setColor(Color.black);
 g.setFont(f);
-drawCenteredString("133006"
-		+ "133007"
-		+ "133008"
 		+ "133009", d.width, d.height, g);
+drawCenteredString("130571"
+, d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
 }
 
