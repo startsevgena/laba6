5dffd2fedb4fe2c4ca5e9a0f5ef60e0953305fa0
chulpan-hasanova <44906579+chulpan-hasanova@users.noreply.github.com>
2018-11-10 01:02:46 +0500

    третий добавил номер ЗК
    Хасанова - 17130161

--- HelloWorld.java
+++ HelloWorld.java
@@ -15,7 +15,7 @@ g.fillRect(0,0,d.width,d.height);
 g.setColor(Color.black);
 g.setFont(f);
 drawCenteredString("17130172"
-		+ "133007"
+		+ "17130161"
 		+ "17130178"
 		+ "133009", d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
