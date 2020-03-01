a0842305e9107d0e880ca0997a5c356787967f7d
Danil <61137958+garbi4@users.noreply.github.com>
2020-03-01 23:56:21 +0500

    Update HelloWorld.java
    добавление зачётки второго участника группы (Гимаева Галия)

--- HelloWorld.java
+++ HelloWorld.java
@@ -14,7 +14,11 @@ g.setColor(Color.white);
 g.fillRect(0,0,d.width,d.height);
 g.setColor(Color.black);
 g.setFont(f);
-drawCenteredString("18130319", d.width, d.height, g);
+drawCenteredString("18130319"
+                   "18130153"
+                   ""
+                   ""
+                   , d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
 }
 
