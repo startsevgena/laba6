1bdd8b896c88d0bab2eace172bbfae1ea5a87ecf
gimops <61138250+gimops@users.noreply.github.com>
2020-03-02 00:04:08 +0500

    Update HelloWorld.java
    добавила свой номер зачётки (Гимаева Галия)

--- HelloWorld.java
+++ HelloWorld.java
@@ -16,7 +16,7 @@ g.setColor(Color.black);
 g.setFont(f);
 drawCenteredString("18130319"
                    "19131142"
-                   ""
+                   "18130153"
                    ""
                    , d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
