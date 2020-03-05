612c8ab928afa710d6e471f258060023a7cffabb
Merci666 <56912568+Merci666@users.noreply.github.com>
2020-03-05 14:14:56 +0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -14,7 +14,9 @@ g.setColor(Color.white);
 g.fillRect(0,0,d.width,d.height);
 g.setColor(Color.black);
 g.setFont(f);
-drawCenteredString("12345", d.width, d.height, g);
+drawCenteredString("12345"
+                   "19131142"
+                   , d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
 }
 
