1862bfea26a5e512111695ad20b767209d97c84c
microlab <microlab@26.36.203.164>
2020-06-01 18:35:31 +0500

    test 4,5

---
+++ src/test/java/laba/Test5.java
@@ -0,0 +1,18 @@
+package laba;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Test5 {
+
+	@org.junit.jupiter.api.Test
+	public void test() {
+		int a = 5;
+		int b = 6;
+		int testValue = 4;
+		int testingValue = 0;
+		testingValue = a * b;
+		assertEquals(testValue,  testingValue);
+		assertTrue(testValue == testingValue);
+	}
+
+}
