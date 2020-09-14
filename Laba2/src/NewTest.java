4e13764528e34831c31e43ee1c59cffb62a047ce
Kuroshup <Kuroshup@192.168.0.10>
2020-09-14 21:59:55 +0500

    Произведено выполнение Лабораторной работы №2

---
+++ Laba2/src/NewTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NewTest {
+
+	@Test
+	void test() {
+		assertEquals("Проверяем номер зачетки",18170252,18170252);
+		assertEquals("Проверяем номер зачетки",18130467,18130467);
+		assertEquals("Проверяем номер зачетки",17130706,17130706);
+	}
+
+}
